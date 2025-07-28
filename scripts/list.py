c2ad6061436903c4a03cd8ee98c7c5acbbe220c2
Torfkopp <mario@fokken.cc>
2025-07-28 15:36:27 +0200

    Improve scripts and pokemon list

--- scripts/list.py
+++ scripts/list.py
@@ -1,16 +1,13 @@
 import csv
 
-i = 0
-names = []
-
 with open('data/pokemon.txt', 'r') as file:
-    names = file.read().splitlines()
+    german_names = file.read().splitlines()
 
 with open('data/list.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     
-    for name in names:
+    for i, name, gname in (x.split(",") for x in german_names):
         filename = name.lower().replace(' ', "-").replace('_', '-').replace('.', "").replace('\'', "").replace(":", "")
-        writer.writerow([name, filename])
+        writer.writerow([name, gname, filename])
 
     
